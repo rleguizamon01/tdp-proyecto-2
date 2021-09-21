efeb11c06a62470038f0f376131c7477315596f8
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-09-21 09:24:07 -0300

    Métodos de juego creados

--- src/logica/Juego.java
+++ src/logica/Juego.java
@@ -1,5 +1,59 @@
 package logica;
 
 public class Juego {
-
+	protected int puntaje;
+	protected int dificultad;
+	protected GUI miGui;
+	protected Grilla miGrilla;
+	protected Reloj miReloj;
+	
+	public Juego(GUI gui) {
+		this.miGui = gui;
+		this.miGrilla = new Grilla();
+		this.miReloj = new Reloj();
+	}
+	
+	public void iniciarPartida() {
+		this.miReloj.run();
+	}
+	
+	public void finalizarPartida() {
+		this.miReloj.setSePuedeJugar(false);
+	}
+	
+	public void pedirRotarDerecha() {
+		this.miGrilla.rotarDerecha();
+	}
+	
+	public void pedirRotarIzquierda() {
+		this.miGrilla.rotarIzquierda();
+	}
+	
+	public void pedirMoverIzquierda() {
+		this.miGrilla.moverIzquierda();
+	}
+	
+	public void pedirMoverDerecha() {
+		this.miGrilla.moverDerecha();
+	}
+	
+	public void pedirMoverAbajo() {
+		this.miGrilla.moverAbajo();
+	}
+	
+	public void pedirActualizar(Position pos, String imagePath) {
+		this.miGui.actualizar(pos, imagePath);
+	}
+	
+	public void actualizarTiempo(String tiempoNuevo) {
+		this.miReloj.setIntervalo(tiempoNuevo);
+	}
+	
+	public void actualizarPuntaje(int puntajeNuevo) {
+		this.miGui.setPuntaje(puntajeNuevo);
+	}
+	
+	public void actualizarSiguienteTetrimino(String nuevoTetrimino) {
+		this.miGrilla.generarSiguienteTetrimino();
+	}
 }
