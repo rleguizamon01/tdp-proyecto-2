fc3b70829cf23b111319b7ba49bae78d53ac824f
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-09-21 09:24:16 -0300

    Métodos de reloj creados

--- src/logica/Reloj.java
+++ src/logica/Reloj.java
@@ -1,5 +1,37 @@
 package logica;
 
-public class Reloj {
-
+public class Reloj extends Thread {
+	protected int intervalo;
+	protected boolean sePuedeJugar;
+	protected Juego miJuego;
+	protected int tiempoTranscurrido; // ?
+	
+	public Reloj(Juego miJuego) {
+		this.miJuego = miJuego;
+		this.intervalo = 1000;
+		this.sePuedeJugar = true;
+	};
+	
+	public void run() {
+		try {
+			while(this.sePuedeJugar) {
+				Thread.sleep(intervalo);
+				this.miJuego.pedirMoverAbajo();		
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setIntervalo(int intervalo) {
+		this.intervalo = intervalo;
+	}
+	
+	public void setSePuedeJugar(boolean puedeJugar) {
+		this.sePuedeJugar = puedeJugar;
+	}
+	
+	private int getTiempoTranscurrido() { // ?
+		return this.tiempoTranscurrido;
+	}
 }
